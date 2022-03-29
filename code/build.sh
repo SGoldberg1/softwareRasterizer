7631e845c66ffd04b81403a91b95c7fc13521182
sgoldberg18 <sg41688@gmail.com>
2022-03-28 22:01:35 -0600

    Included build script
    Forgot to add build...

--- code/build.sh
+++ code/build.sh
@@ -5,6 +5,21 @@ platformFlags='-rdynamic'
 commonFlags='-O0 -Wall -Wno-missing-braces -Wno-writable-strings'
 platformLibraries='-lSDL2'
 
-clang++ $debugFlags $commonFlags $engineFlags ../code/Engine.cpp -o ../build/engine.so
+ME="$(readlink -f "$0")"
+CODE_HOME="$(dirname "$ME")"
 
-clang++ $commonFlags $debugFlags $platformFlags ../code/Linux_Main.cpp -o ../build/main $platformLibraries
+SOURCE="$1"
+if [ -z "$SOURCE" ]; then
+    SOURCE="$(readlink -f "$CODE_HOME/Linux_Main.cpp")"
+fi
+
+#echo ME = $ME
+#echo SOURCE = $SOURCE
+#echo CODE_HOME = $CODE_HOME
+
+clang++ $debugFlags $commonFlags $CODE_HOME/assetProcessor/AssetProcessor_Main.cpp -o $CODE_HOME/assetProcessor/assetProcessor
+"$CODE_HOME/assetProcessor/assetProcessor" "$CODE_HOME"
+
+clang++ $debugFlags $commonFlags $engineFlags $CODE_HOME/Engine.cpp -o ../build/engine.so
+
+clang++ $commonFlags $debugFlags $platformFlags $SOURCE -o ../build/main $platformLibraries
